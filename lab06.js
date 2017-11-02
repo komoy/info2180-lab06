e7ddfbb9b077f5fbc0cf4c821ff08de3f5332102
Komoy Haye <komoyhaye16@gmail.com>
2017-11-02 03:27:57 +0000

    exercise 2

---
+++ lab06.js
@@ -0,0 +1,22 @@
+// JavaScript File
+window.onload=function(){
+let search=document.getElementById("search");
+search.onclick= function()
+{
+  let ajax = new XMLHttpRequest();
+  let word=document.getElementById("word");
+  var url='request.php?q='+word.value;
+   ajax.onreadystatechange = function()
+   {
+       if(ajax.readyState==4 && ajax.status==200){
+       alert(document.getElementById("result").innerHTML = this.responseText);
+       
+        }
+        
+   };
+   
+   
+        ajax.open("GET",url, true);
+        ajax.send();
+};
+};
